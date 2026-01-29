890a2fb0aa86afa358dca01ef2d0fe0ca0c23caa
Amit Gupta <amitgupta4263@gmail.com>
2026-01-29 16:35:15 +0530

    Delete DSA Java/oops4/tempCodeRunnerFile.java

--- DSA Java/oops4/tempCodeRunnerFile.java
+++
@@ -1,7 +0,0 @@
-public static void main(String[] args){
-    //     LearnInnerClass obj = new LearnInnerClass();
-    //     Toy toy = obj.new Toy();
-    //     toy.price = 45; 
-
-    //     PlayStation playstation =new LearnInnerClass.PlayStation();
-    // }
