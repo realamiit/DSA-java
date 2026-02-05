9f1400b4682dc8dcba25b9febb85f6ca893184f2
Amit Gupta <your@email.com>
2026-02-06 01:37:04 +0530

    Update exception handling and generics practice code

--- DSA Java/GenericsAndWrapperClasses/GenericMethods.java
+++ DSA Java/GenericsAndWrapperClasses/GenericMethods.java
@@ -1,11 +1,34 @@
 public class GenericMethods {
     public static void main(String[]args){
 
-        printData("Hello Im amit gupta");
+        // here we take data
+        // printData("Hello Im amit gupta");  //String
+        printData(23445);                  //Integer    
+        // printData(true);                   //Boolean 
+        // printData(false);                  //Boolean 
+        // printData("Amit");                  //String
+
+
+         GenericMethods obj = new GenericMethods();
+         obj.doubleData("Amit Gupta");
+         obj.doubleData(12435);
+
+         CustomClass custom = new CustomClass();
+         obj.doubleData(custom);
     }
 
-    static void printData(String data){
+   
+
+    static<E extends Number> void printData(E data){   //After (E) put this we can also Pass any type of DataType 
         System.out.println(data);
     }
+
+    <E> void doubleData(E data){
+        System.out.println(data);
+    }
+
+}
+
+class CustomClass{
 
 }
