f97b1800c938ccc1c53d9fe229e19f832750feb8
Amit Gupta <your@email.com>
2026-02-11 00:20:59 +0530

    Add latest Java practice files

---
+++ DSA Java/collectionQueueAndSet/LearnQueue.java
@@ -0,0 +1,43 @@
+// imoprt java.util.*;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class LearnQueue {
+    public static void main(String[]args){
+        Queue <Integer> queue = new LinkedList<>();
+
+////         it throw Execption also 
+        // queue.add(10);
+        // queue.add(20);
+        // queue.add(30);
+        // queue.add(40);
+        // queue.add(60);
+
+//       both are same work
+
+//// offer return Boolean & don't return Execption also
+
+        queue.offer(80);
+        queue.offer(90);
+        queue.offer(100);
+        queue.offer(110);
+
+
+        System.out.println(queue);
+
+        // System.out.println(queue.poll());
+        // System.out.println(queue);
+
+        // System.out.println(queue.peek());
+        // System.out.println(queue);
+
+
+        while(!queue.isEmpty()){
+            System.out.println(queue.poll());
+        }
+        
+
+
+    }
+
+}
---
+++ DSA Java/collectionQueueAndSet/tempCodeRunnerFile.java
@@ -0,0 +1,4 @@
+queue.offer(80);
+        queue.offer(90);
+        queue.offer(100);
+        queue.offer(110);
