cdccb081eee6d9d87cff4e008bb9e5991eb2d0f3
Amit Gupta <your@email.com>
2026-02-12 00:39:35 +0530

    Add latest Java practice files

---
+++ DSA Java/CollectionAndList/LearnpriorityQueue.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class LearnpriorityQueue {
+    public static void main(String[]args){
+        Queue<Integer> pq = new PriorityQueue<>();
+
+        pq.add(10);
+        pq.add(25);
+        pq.add(33);
+        pq.add(55);
+
+        System.out.println(pq);
+    }
+
+}
