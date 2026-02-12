48676352ae44ac322a79dcd67ac480e6e5ec1cca
Amit Gupta <your@email.com>
2026-02-12 16:41:14 +0530

    Add latest Java collection practice files

--- DSA Java/collectionQueueAndSet/HashSetOfCostomObjects.java
+++ DSA Java/collectionQueueAndSet/HashSetOfCostomObjects.java
@@ -1,23 +1,33 @@
 import java.util.*;
 
 // this is a normal Class in java 
-class Student{
-    int rollNo;
-    String Name;
+class Student {
 
-    public Student(int rollNo, String Name){
-        this.rollNo = rollNo;
-        this.Name = Name;
+    int id;
+    String name;   // ✅ YOU ARE MISSING THIS
+
+    Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
+
 public class HashSetOfCostomObjects {
     public static void main(String[]args){
-        Set<String> set = new HashSet<>();
+        Set<Student> set = new HashSet<>();
 
-        set.add("Amit");
-        set.add("Ayush");
-        set.add("Nirman");
-        set.add("Amit");
+        set.add(new Student(3, "Amit"));
+        // set.add(new Student(3, "Ayush"));
+        // set.add(new Student(3, "Nirman"));
+        set.add(new Student(3,"Amit"));
 
         System.out.println(set);
 
