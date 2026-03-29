5d9392a538d16fb5bb494e71137f3ca571d3d049
Amit Gupta <amitgupta4263@gmail.com>
2026-03-29 10:46:16 +0530

    Add FindSubstringInAString.java to implement substring search functionality

---
+++ DSA Java/StringPractice/FindSubstringInAString.java
@@ -0,0 +1,13 @@
+public class FindSubstringInAString {
+    public static void main(String[] args) {
+        String s1 = "follobuilding.programamit";
+        String s2 = "building.program";
+
+        if(s1.contains(s2)){
+            System.out.println("yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+
+}
