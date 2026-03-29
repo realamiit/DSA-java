226c339e7d595cdd86a2a1202aeee1ed10ba373d
Amit Gupta <amitgupta4263@gmail.com>
2026-03-29 10:53:28 +0530

    Add ExecptionHandling.java to demonstrate exception handling in Java

---
+++ DSA Java/GenericsAndWrapperClasses/learnexecptionHandling/ExecptionHandling.java
@@ -0,0 +1,18 @@
+public class ExecptionHandling {
+
+    public static void main(String[]args){
+
+        int a[] =  new int[5];
+        System.out.println("Hello Varanasi");
+
+        try{
+            System.out.println(a[2]);
+        }catch(Exception e){                    // this if u don't want to putin your
+                                              //  programm thsn no nee to usee also
+            System.out.println("Exception Handled");
+        } finally{
+            System.out.println("I will run always");
+        }
+        System.out.println("Beeeyeee World");
+    }
+}
