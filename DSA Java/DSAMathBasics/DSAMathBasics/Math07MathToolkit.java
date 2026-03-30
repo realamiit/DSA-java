d001ca0cae404e2faa5447d49928f75d423d8dba
Amit Gupta <amitgupta4263@gmail.com>
2026-03-30 23:55:59 +0530

    Add Math07MathToolkit.java for GCD and LCM calculations using user input

---
+++ DSA Java/DSAMathBasics/DSAMathBasics/Math07MathToolkit.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Math07MathToolkit {
+
+    // GCD using Euclidean Algorithm
+    static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    // LCM using GCD
+    static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("===== Math Toolkit =====");
+        System.out.println("1. Find GCD");
+        System.out.println("2. Find LCM");
+        System.out.print("Choose option: ");
+        int choice = sc.nextInt();
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("GCD: " + gcd(a, b));
+                break;
+            case 2:
+                System.out.println("LCM: " + lcm(a, b));
+                break;
+            default:
+                System.out.println("Invalid choice!");
+        }
+
+        sc.close();
+    }
+}
+
+
+
+
+
