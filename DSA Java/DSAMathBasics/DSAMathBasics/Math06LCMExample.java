47f4072c5f97ca387a1f9bd965995d8178c1eae3
Amit Gupta <amitgupta4263@gmail.com>
2026-03-30 23:55:48 +0530

    Add Math06LCMExample.java and Math07MathToolkit.java for LCM and GCD calculations

---
+++ DSA Java/DSAMathBasics/DSAMathBasics/Math06LCMExample.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Math06LCMExample {
+    // Java Code (LCM using GCD)
+
+    static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("LCM: " + lcm(a, b));
+
+        sc.close();
+    }
+}
