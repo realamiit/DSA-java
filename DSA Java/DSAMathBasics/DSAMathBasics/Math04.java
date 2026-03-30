dac6f39dd763e6bdddae21c35523763989d7756b
Amit Gupta <amitgupta4263@gmail.com>
2026-03-30 23:46:01 +0530

    Add Math04.java for basic mathematical operations demonstration

---
+++ DSA Java/DSAMathBasics/DSAMathBasics/Math04.java
@@ -0,0 +1,10 @@
+public class Math04 {
+ public static void main(String[] args) {
+
+        System.out.println("Square root: " + Math.sqrt(25));
+        System.out.println("Power: " + Math.pow(2, 3));
+        System.out.println("Max: " + Math.max(10, 20));
+        System.out.println("Min: " + Math.min(10, 20));
+        System.out.println("Random: " + Math.random());
+    }
+}
