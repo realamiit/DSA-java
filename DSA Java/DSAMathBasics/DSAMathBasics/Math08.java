0af59c1708b657ee74897825716b15b118bad443
Amit Gupta <amitgupta4263@gmail.com>
2026-04-04 10:42:39 +0530

    the answer is uunexoected

--- DSA Java/DSAMathBasics/DSAMathBasics/Math08.java
+++ DSA Java/DSAMathBasics/DSAMathBasics/Math08.java
@@ -1,9 +1,17 @@
 public class Math08 {
     public static void main(String[] args){
+        // Defining Veriables
         int num1 = 12345689;
         int num2 = 1000;
 
+        //printing Output
         System.out.println(num1+num2);
     }
 
 }
+
+
+// complexity
+// Time => O(1)
+// Space => O(1)
+// because its constant
---
+++ DSA Java/DSAMathBasics/DSAMathBasics/Math09.java
@@ -0,0 +1,14 @@
+public class Math09 {
+    public static void main(String[] args) {
+        // Defining Veriables
+        int num1 = -24327354;
+        int num2 = 10;
+
+        //performing Substraction
+        int ans = (num1-num2);
+
+        // printing Output
+        System.out.println(ans);
+    }
+
+}
