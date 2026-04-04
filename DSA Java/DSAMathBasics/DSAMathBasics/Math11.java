1b763fc88250151a08bcf4e6ff00ecf71da342a6
Amit Gupta <amitgupta4263@gmail.com>
2026-04-04 11:38:57 +0530

    Update Math11 class to correct integer division example and add additional calculations

--- DSA Java/DSAMathBasics/DSAMathBasics/Math11.java
+++ DSA Java/DSAMathBasics/DSAMathBasics/Math11.java
@@ -1,11 +1,23 @@
 public class Math11 {
     public static void main(String[] args){
-        int num1 = 5;
-        int num2 = 2;
+        int num1 = 7;
+        int num2 = 3;
 
-        int num = num1 / num2;
-
+        int num = num1 / num2;  // the actual  output is 2.5 in mathemetical way but it return 2 because 
+        //                        we insert intiger valu and this is (2.5) float value
         System.out.println(num);
-    }
 
+        int num3 = -2134566432; //INT_MIN
+        int num4 = -1;
+
+        int ans = num3 / num4;
+        System.out.println(ans); 
+    }
 }
+
+// complexity
+// Time => O(1)
+// Space => O(1)
+// because its constant
+
+
