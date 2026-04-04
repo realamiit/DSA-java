9725bbfd999005f824686cfc0d5b24283d354a5c
Amit Gupta <amitgupta4263@gmail.com>
2026-04-04 11:11:26 +0530

    Add Math11 class with main method for integer division

---
+++ DSA Java/DSAMathBasics/DSAMathBasics/Math11.java
@@ -0,0 +1,11 @@
+public class Math11 {
+    public static void main(String[] args){
+        int num1 = 5;
+        int num2 = 2;
+
+        int num = num1 / num2;
+
+        System.out.println(num);
+    }
+
+}
