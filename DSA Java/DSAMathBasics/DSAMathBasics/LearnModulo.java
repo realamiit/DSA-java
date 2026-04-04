486b952058b9918550a62f41729bce1423e66db0
Amit Gupta <amitgupta4263@gmail.com>
2026-04-04 12:09:47 +0530

    we will learn inn this classs how modulo works in programms

---
+++ DSA Java/DSAMathBasics/DSAMathBasics/LearnModulo.java
@@ -0,0 +1,14 @@
+public class LearnModulo {
+    public static void main(String[] args) {
+        int num1 = 8;
+        int num2 = 5;
+
+        int ans = num1 % num2;
+        System.out.println(ans);
+    }
+
+}
+
+// Time Complexity => O(1)
+// Space Complexity => O(1)
+// because its constant
