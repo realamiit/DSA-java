86e3b0c2763d32e5fee50e5c6511f3f519b94d54
Amit Gupta <amitgupta4263@gmail.com>
2026-04-05 13:33:11 +0530

    Refactor package name and improve code formatting
    Updated package name to 'QuestionsOfDSA' and fixed formatting issues.

--- DSA Java/DSAMathBasics/DSAMathBasics/QuestionsOfDsa/SumOfDigits.java
+++ DSA Java/DSAMathBasics/DSAMathBasics/QuestionsOfDsa/SumOfDigits.java
@@ -1,7 +1,25 @@
-package QuestionsOfDsa;
+package QuestionsOfDSA;
 
 public class SumOfDigits {
+    package QuestionsOfDsa;
+
+public class SumOfDigits {
+
+    public static void sumOfDigits(int num) {
+        int sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10; // extract last digit
+            sum += digit;         // add to sum
+            num /= 10;            // remove last digit
+        }
+
+        System.out.println("Sum of digits = " + sum);
+    }
+
     public static void main(String[] args) {
+        int number = 23456788;
+        sumOfDigits(number);
         
     }
 
