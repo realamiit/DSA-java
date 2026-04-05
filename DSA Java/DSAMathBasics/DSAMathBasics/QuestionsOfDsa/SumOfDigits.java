402f62471858826eda884cb7afac79d63284c873
Amit Gupta <amitgupta4263@gmail.com>
2026-04-05 13:33:24 +0530

    Fix package name and reorganize SumOfDigits class structure

--- DSA Java/DSAMathBasics/DSAMathBasics/QuestionsOfDsa/SumOfDigits.java
+++ DSA Java/DSAMathBasics/DSAMathBasics/QuestionsOfDsa/SumOfDigits.java
@@ -1,8 +1,21 @@
-package QuestionsOfDsa;
+package QuestionsOfDSA;
 
 public class SumOfDigits {
-    public static void main(String[] args) {
-        
+
+    public static void sumOfDigits(int num) {
+        int sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10; // extract last digit
+            sum += digit;         // add to sum
+            num /= 10;            // remove last digit
+        }
+
+        System.out.println("Sum of digits = " + sum);
     }
 
+    public static void main(String[] args) {
+        int number = 23456788;
+        sumOfDigits(number);
+    }
 }
