39e5f2b93a01201947fae55912b1d9bb32ab803b
Amit Gupta <amitgupta4263@gmail.com>
2026-04-08 10:55:11 +0530

    Refactor ReverseNumber class by commenting out unused methods and main function

--- DSA Java/DSAMathBasics/DSAMathBasics/QuestionsOfDsa/ReverseNumber.java
+++ DSA Java/DSAMathBasics/DSAMathBasics/QuestionsOfDsa/ReverseNumber.java
@@ -1,32 +1,43 @@
 package QuestionsOfDSA;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class ReverseNumber {
-    static int reverse(int num) {
-        int rev = 0;
+    // static int reverse(int num) {
+    //     int rev = 0;
 
-        while (num > 0) {
-            int digit = num % 10;     // last digit
-            rev = rev * 10 + digit;  // build reverse
-            num = num / 10;          // remove last digit
-        }
+    //     while (num > 0) {
+    //         int digit = num % 10;     // last digit
+    //         rev = rev * 10 + digit;  // build reverse
+    //         num = num / 10;          // remove last digit
+    //     }
 
-        return rev;
-    }
+    //     return rev;
+    // }
 
-    public static void main(String[] args) {
+    // public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+    //     Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a number: ");
-        int num = sc.nextInt();
+    //     System.out.print("Enter a number: ");
+    //     int num = sc.nextInt();
 
-        int result = reverse(num);
+    //     int result = reverse(num);
 
-        System.out.println("Reversed Number: " + result);
+    //     System.out.println("Reversed Number: " + result);
 
-        sc.close();
+    //     sc.close();
+    // }
+
+    public static int reverseNum(int num) {
+        int reversedNum = 0;
+         while(num > 0){
+            int digit = num % 10;
+            reversedNum = reversedNum * 10 + digit;
+
+            num = num / 10;
+         }
+         return reversedNum;
     }
 
 }
