9a81f17a1028548ada9c3ab5492fae0f3228437e
Amit Gupta <amitgupta4263@gmail.com>
2026-05-08 23:30:48 +0530

    Fix loop condition in Pattern04.java to correctly print the pattern

--- DSA Java/Patterns/Pattern04.java
+++ DSA Java/Patterns/Pattern04.java
@@ -2,7 +2,7 @@
 public class Pattern04 {
     public static void main(String[] args) {
         for(int i=0; i<10; i++){
-            for(int j=0; j<i-1; j++){
+            for(int j=0; j<i+1; j++){
                 System.out.print("*");
             }
             System.out.println(" ");
