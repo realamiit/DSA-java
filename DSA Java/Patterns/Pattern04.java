7f05d892f331e4dd583b10daa179570647bff5d1
Amit Gupta <amitgupta4263@gmail.com>
2026-05-08 23:32:57 +0530

    Refactor loop conditions in Pattern04.java for correct pattern printing

--- DSA Java/Patterns/Pattern04.java
+++ DSA Java/Patterns/Pattern04.java
@@ -1,8 +1,9 @@
 
 public class Pattern04 {
     public static void main(String[] args) {
-        for(int i=0; i<10; i++){
-            for(int j=0; j<i+1; j++){
+        int n = 8;
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<i-1; j++){
                 System.out.print("*");
             }
             System.out.println(" ");
