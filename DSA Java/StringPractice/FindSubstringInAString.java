e89b2bba9ced909fe238d0d486be185f9acce78d
Amit Gupta <amitgupta4263@gmail.com>
2026-05-14 23:16:29 +0530

    Update substring check output messages

--- DSA Java/StringPractice/FindSubstringInAString.java
+++ DSA Java/StringPractice/FindSubstringInAString.java
@@ -1,13 +1,13 @@
 public class FindSubstringInAString {
     public static void main(String[] args) {
-
+//  it means substring is Avilable on String also than the output is true than Flass ()no
         String s1 = "follobuilding.programamit";
         String s2 = "building.program";
 
         if(s1.contains(s2)){   //Check karta hai ki s2 continuous substring hai ya nahi
-            System.out.println("yes");
+            System.out.println("True");
         }else{
-            System.out.println("No");
+            System.out.println("False");
         }
     }
 
