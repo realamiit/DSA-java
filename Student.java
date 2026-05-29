df54ba59d734e33bf6f1e9f1d1cbf5d0556f767c
Amit Gupta <amitgupta4263@gmail.com>
2026-05-29 16:22:23 +0530

    Add student class for sure learn it

--- Student.java
+++ Student.java
@@ -4,14 +4,15 @@ public class Student {
     String Name;
     int rollNo;
     int marks;
+    String result;
 
     // ye method hai
 
     void introduce() {
-        System.out.println("mera Name : " + Name + " , mera rollno : , " +rollNo + " , mera marks : , " + marks);
+        System.out.println("mera Name : " + Name + " , mera rollno : , " +rollNo + " , mera marks : , " + marks + ", My result" + result);
     }
 
-    // main object class hai ye
+    // main object lass hai ye
     public static void main(String[] args) {
         Student s = new Student(); // object is here
         Student s1 = new Student();
@@ -19,6 +20,7 @@ public class Student {
         s.Name = "Amit"; // this is object
         s.rollNo = 22;
         s.marks = 78;
+        s.result = 
 
         s1.Name = "Ashish";
         s1.rollNo = 31;
@@ -46,5 +48,13 @@ public class Student {
 // yha to main object call huyi hai mtlb hm object ko yha call krte ha after main() Student s = new Student (); 
 // than we classs there data in this method for the emplemaintation thats is 
 // after all we go with single line also  
+// object hai jo uskkuch aise smjhoo jaise tum koyii tumhare aas paas phone pen paper and bottel hoga than ye sb hoga ab ise kuch aise smjh tumhara phone tumhara hai ye object huva 
+//  aur phone class huva 
+// suno suno iski help se smjho :  Student s = new Student();   Student =>  kon si class hai mtlb ye class hai jo question kr rhi hai for sure ki mai class hu kon si xclass 
+// S =>  ye eek object hai jisko name diya gya hai we can change it also mtlb name diya hai object ko 
+// new Student =>  ye btata hai ki nya object creat huva hai jo ki jiska name hai s also 
+// for better nunderstanding jaise hmne do alg alg object bnayi hai vaise hi hm alg alg object bnayi hai vaise hi Amit And Ashish dono alg alg student 
+// hai inki alg alg deaitls hogi bhle classs eeek hii hoo 
+// ye matter nhi krti they have there own different property many more things
 
-// after doing this we call object here for the output s.intreoduce();
+//  for sure after doing this we call object here for the output s.intreoduce();  yha object call ho rha hai jaise for the shon there output 
