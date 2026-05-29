23c0820dc771c2557465d5167c1fda27edc98dd6
Amit Gupta <amitgupta4263@gmail.com>
2026-05-30 02:02:21 +0530

    learn do while

---
+++ Day1/topic5.java
@@ -0,0 +1,14 @@
+package Day1;
+
+public class topic5 {
+    public static void main(String[] args) {
+        int num = 10;
+
+        for(int i = 1; i <=num ;i++ ){
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+
+}
