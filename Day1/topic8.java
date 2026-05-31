bb422e59027279d9389010a7f5b9d8cff5cf2e9c
Amit Gupta <amitgupta4263@gmail.com>
2026-05-31 16:27:26 +0530

    print 5 student marks uding loop

--- Day1/topic8.java
+++ Day1/topic8.java
@@ -2,9 +2,23 @@ package Day1;
 public class topic8 {
     public static void main(String[] args) {
         int[] marks = {90,88,76,98,70};
-        for (int i = 1; i < marks.length; i++){
+        for (int i = 0; i < marks.length; i++){
             System.out.println("Student "+(i+1) + " ka marks : "+marks[i]);
         } 
     }
 
 }
+
+
+// pehele int me arrry liya jo marks hai 5 alag alg student ka for the printing five diferrerent student ko than
+//  go with the loop for the printing thee ouptut and cheak the logic to represent for 5 time to present and 
+//   int this for loop we take int i = zero (0) kyuki hmje zero se array ko print krwana hao thats why we ta\ke it one than gave semicollun 
+// than we gave threre  again teke i < greaatre than marks .length kyuki hme array ki length  print kwr==rwaanui hai 
+//  than close wuith the semicolun and g go for i++ to increase the input vakue aftere true  the condatioon for sure  go withb curly brackate 
+// in the curly bracket we go for sout system .out .println (in thisFirst "Student "+ for add more things (i+1) for inreaserr the thr student count  + " ka marks "+ marks[i] );
+// output is :
+// Student 1 ka marks : 90  
+// Student 2 ka marks : 88
+// Student 3 ka marks : 76
+// Student 4 ka marks : 98
+// Student 5 ka marks : 70
