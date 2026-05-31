297d512a83241135240b380de538cc194f6c5f1f
Amit Gupta <amitgupta4263@gmail.com>
2026-05-31 16:24:06 +0530

    learn and revise String in some components

---
+++ Day1/topic9.java
@@ -0,0 +1,24 @@
+package Day1;
+
+import java.util.Scanner;
+// import java.util.*;
+
+public class topic9 {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+
+        System.out.println("Enter Your Name");
+        String name = sc.nextLine();
+
+        System.out.println(name);
+        System.out.println(name.length());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.charAt(0));
+        
+        
+    }
+
+}
+
+
+//  pehele 
