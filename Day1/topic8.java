ef84029e4b66f60b1ffca344ba6eacd99c7d73f4
Amit Gupta <amitgupta4263@gmail.com>
2026-05-31 14:17:00 +0530

    fix arry

---
+++ Day1/topic8.java
@@ -0,0 +1,10 @@
+package Day1;
+public class topic8 {
+    public static void main(String[] args) {
+        int[] marks = {90,88,76,98,70};
+        for (int i = 1; i < marks.length; i++){
+            System.out.println("Student "+(i+1) + " ka marks : "+marks[i]);
+        } 
+    }
+
+}
