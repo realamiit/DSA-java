61a594c1484b9848f2af8568a867159a475abe81
Amit Gupta <amitgupta4263@gmail.com>
2026-06-03 13:12:38 +0530

    Update Car details

--- DSA Java/Revise/Day1/car.java
+++ DSA Java/Revise/Day1/car.java
@@ -1,15 +1,27 @@
 package Day1;
 
-public class car {
-    String Brand;
-    String Color;
-    int Speed;
+public class Car {
+    String brand;
+    String color;
+    int speed;
 
-    car C = new car();
+    Car(String Brand, String Color, int Speed) {
+        this.brand = Brand;
+        this.color = Color;
+        this.speed = Speed;
+    }
 
+    void details() {
+        System.out.println("Brand : " + brand + "Color : " + color + "Speeed : " + speed);
+    }
 
     public static void main(String[] args) {
-        
+        Car c1 = new Car("Maruti", "Red", 120);
+        Car c2 = new Car("Honda", "Brown", 130);
+
+        c1.details();
+        c2.details();
+
     }
 
 }
