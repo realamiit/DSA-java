5b34daa8911aa8b796ae2ff2af6defba78c15c57
Amit Gupta <amitgupta4263@gmail.com>
2026-06-04 01:50:45 +0530

    use of Split(); to count the spaces aaye vhaaa se tood do aur kitne chunk bne return kr do

---
+++ DSA Java/Revise/Day1/topic16.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class topic16 {
+    public static void main(String[] args) {
+        String arr = "Hello world";
+
+        String[] words = arr.split(" "); 
+        System.out.println(words.length);
+    }
+    
+}
