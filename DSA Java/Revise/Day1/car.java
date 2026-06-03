45d35a135b1707b8f6c43696ba05f93e11e1ec60
Amit Gupta <amitgupta4263@gmail.com>
2026-06-03 12:37:23 +0530

    OOps in constructor

---
+++ DSA Java/Revise/Day1/car.java
@@ -0,0 +1,15 @@
+package Day1;
+
+public class car {
+    String Brand;
+    String Color;
+    int Speed;
+
+    car C = new car();
+
+
+    public static void main(String[] args) {
+        
+    }
+
+}
