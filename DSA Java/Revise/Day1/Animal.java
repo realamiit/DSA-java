6de9a4b899d443da35410d5d4b845642a3c02082
Amit Gupta <amitgupta4263@gmail.com>
2026-06-04 16:04:50 +0530

    name And Super.name Summerise this

--- DSA Java/Revise/Day1/Animal.java
+++ DSA Java/Revise/Day1/Animal.java
@@ -19,11 +19,26 @@ class Dog extends Animal {
         super.eat();
 
     }
+
     public static void main(String[] args) {
         Dog d = new Dog();
         d.show();
     }
 }
 
+// main super ko use kiya aur name Animal void eat bnaya uske und "Animal kha
+// rha hai "
+// prin krwaya this is the features we can exttend by the using super key word
+// class Dog Extend Animal ko extends Animal fir 
+// usme String name bnaya dog name ka fir usme  viud show variable bnaya priint krwaya 
+//  name print krwaya and fir again Super.name call kiya fir inme Supern.eat();
 
-// main super ko use kiya 
+// main me Dog d = new Dog();
+// d.show();
+// bnaya  upr se show ko call kiya useme fir call huva void show(); ko call kiya name ,
+// super.name , super.eat();
+
+// tha after call this we get the output is : 
+//                                             Dog
+//                                             Animal
+//                                             Animal kha rha hai
