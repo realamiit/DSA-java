042b8e3d5e7fbbebab5bc40c147b2add66ddd782
Amit Gupta <amitgupta4263@gmail.com>
2026-06-05 18:12:38 +0530

    fix error in this particular Recursion

--- DSA Java/Recursion/p1.java
+++ DSA Java/Recursion/p1.java
@@ -1,18 +1,16 @@
 public class p1 {
 
-    
-    void countdown(int n){
-            if(n==0){
-                System.out.println("Done is");
-                return;
-            }
-        }
-    public static void main(String[] args) {
-        // int countdown;
-        int n=5;
-        System.out.println(5);
-        countdown(n-1);
-        
+    void print(int n) {
+        if (n == 0) // base case : bs yhi rokna hai
+            return;
+
+        print(n - 1);
+        System.out.println(n);
     }
 
+    public static void main(String[] args) {
+        p1 obj = new p1(); // object bnaya method Static nhi hai
+        obj.print(5);
+
+    }
 }
