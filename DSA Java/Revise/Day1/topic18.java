13b7eb809627f1792b378748524030edea18308e
Amit Gupta <amitgupta4263@gmail.com>
2026-06-06 13:47:46 +0530

    🤣 agn  i -- for"

--- DSA Java/Revise/Day1/topic18.java
+++ DSA Java/Revise/Day1/topic18.java
@@ -3,7 +3,7 @@ package Day1;
 public class topic18 {
     public static void main(String[] args) {
        String arr = "Hello world";
-        for(int i = arr.length()-1; i>=0; i++){
+        for(int i = arr.length()-1; i>=0; i--){
         System.out.print(arr.charAt(i));
             
         }
@@ -13,7 +13,8 @@ public class topic18 {
 
 }
 
-// to reverse te array this merhod apply  
+// to reverse the String this merhod apply  
+// character ko reverse kiya the approch is 
  //complexicty 
 //   Time : O(n) ==> character baar baar print  ho rha hai 
 // space : O(1)  ==> extra space nhi liya 
