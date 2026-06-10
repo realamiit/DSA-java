88db838982996ae648bf91e472502982e49d28c8
Amit Gupta <amitgupta4263@gmail.com>
2026-06-10 17:15:13 +0530

    optimese the code

--- DSALearn/Recursion/DSA/printNumbers.java
+++ DSALearn/Recursion/DSA/printNumbers.java
@@ -1,7 +1,38 @@
 package DSA;
 
 public class printNumbers {
-    if(i == 0) return;
-    
+    public void printNumber(int n){
+    if(n == 0) return;
+    printNumber(n-1);
+    System.out.println(n);
+    }
+    public static void main(String[] args) {
+        printNumbers obj = new printNumbers();
+        obj.printNumber(5);
+    }
 
 }
+
+
+// User input 
+
+// package DSA;
+// import java.util.Scanner;
+// public class printNumbers {
+//     public void printNumber(int n){
+//     if(n == 0) return;
+//     printNumber(n-1);
+//     System.out.println(n);
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter your number");
+//         printNumbers obj = new printNumbers();
+//         int print = sc.nextInt();
+//         obj.printNumber(print);
+//     }
+
+// }
+
+// time complexity : O(2^n) 
+// Space Complexity : O(1)  because no use of extra Spaces
