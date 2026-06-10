e02b5b58aa2cc8df5dafa1a36d93ac58398d69d2
Amit Gupta <amitgupta4263@gmail.com>
2026-06-10 23:04:15 +0530

    update n to x cause we cant get expected value

--- DSALearn/Recursion/DSA/square.java
+++ DSALearn/Recursion/DSA/square.java
@@ -4,7 +4,7 @@ public class square {
     // power (2,5) = 2*2*2*2*2 = 32
     int  power (int x,int n){
     if(n == 0) return 1; // base case
-    return n * power(x, n-1); 
+    return x * power(x, n-1); 
     }
     public static void main(String[] args) {
         square obj = new square();
