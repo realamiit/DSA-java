884c02b523b947899441cfae8abddb2e5b961ba7
Amit Gupta <amitgupta4263@gmail.com>
2026-06-10 23:28:36 +0530

    Summerize this to better understanding

--- DSALearn/Recursion/DSA/square.java
+++ DSALearn/Recursion/DSA/square.java
@@ -1,15 +1,27 @@
 package DSA;
 
 public class square {
-    // power (2,5) = 2*2*2*2*2 = 32
-    int  power (int x,int n){
-    if(n == 0) return 1; // base case
-    return x * power(x, n-1); 
+
+    int  power(int x,int n){
+    if(n == 0) return 1;      // base case
+    return x * power(x, n-1);    //  recursive Function
     }
     public static void main(String[] args) {
         square obj = new square();
          int result = obj.power(2,5);
          System.out.println(result);
     }
-
 }
+
+// (2,5) = 2*2*2*2*2 = 32  this is the Expected ouutput
+// cheak the condation after 
+// shown there base case  ydi n == 0 than return 1 
+// than after done it call recursive Function return x * power(x * n-1); than close tbe brackat
+//  than go to  main and make an object 
+// int result = obj.power the power is the name of method (x:2,n:5 ) aur ye dono bracket me both are parameater
+//  after understanding this 
+//  go for the System.out.println(result) print krwayege 
+//  yha ya in this line System ka matlab hai => basickly ye java ki inbuild class hai aur kuch nhi 
+// out => output stream hai ye bolta hai outputt ke bahr jaoo
+// println => he is always saying ki print if u want somthing and go out of this particular line it means print and go to next line 
+// (result) => the result is also return the those we want to print anything or expected things also
