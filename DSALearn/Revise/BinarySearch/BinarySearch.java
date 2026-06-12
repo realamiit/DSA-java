4d69b3f11fe661661377adc03be368af0ba1d2cf
Amit Gupta <amitgupta4263@gmail.com>
2026-06-13 01:01:12 +0530

    Add new topic and anothe stemp of DSA Binary search in basics

---
+++ DSALearn/Revise/BinarySearch/BinarySearch.java
@@ -0,0 +1,12 @@
+package BinarySearch;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {10 ,45, 7,89,23};
+        int target = 89;
+        for(int i =0; i<= arr.length; i++){
+            System.out.println(arr[i] == target);
+        }
+    }
+
+}
