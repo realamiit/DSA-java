fc37d389d3940390c19c4ad8b12704df812ae1ef
Amit Gupta <amitgupta4263@gmail.com>
2026-06-19 23:52:01 +0530

    ninary srarch fine in entire arr no of index

---
+++ DSALearn/Revise/BinarySearch/BinarySearch2.java
@@ -0,0 +1,17 @@
+package BinarySearch;
+
+public class BinarySearch2 {
+    int[] arr = {5,3,5,8,7,9,5,2};
+    target = 5;
+    public static void main(String[] args) {
+        BinarySearch2 obj = new BinarySearch2();
+        for(int i = 0; i<arr.length; i++){
+            if(obj.arr[i] == terget){
+                return;
+            }
+        }
+        
+
+    }
+
+}
