00a787c159c8012c3311a7a107b9858e2b0346e3
Amit Gupta <amitgupta4263@gmail.com>
2026-06-21 00:25:19 +0530

    fix all the bug and shown the output

--- DSALearn/Revise/BinarySearch/BinarySearch2.java
+++ DSALearn/Revise/BinarySearch/BinarySearch2.java
@@ -1,19 +1,21 @@
 package BinarySearch;
 
 public class BinarySearch2 {
-    int[] arr = {5,3,5,8,7,9,5,2};
+    int[] arr = { 5, 3, 5, 8, 7, 9, 5, 2,5 };
     int target = 5;
+
     public static void main(String[] args) {
         BinarySearch2 obj = new BinarySearch2();
-        for(int i = 0; i<obj.arr.length; i++){
-            if(obj.arr[i] == obj.target){
-                return;
-                int count = 0;
-                
+        int count = 0;
+        for (int i = 0; i < obj.arr.length; i++) {
+            if (obj.arr[i] == obj.target) {
+                count++;
             }
-        }
-        
 
+        }
+        System.out.println(count);
     }
 
 }
+
+// 
---
+++ DSALearn/mainfile/main1.java
@@ -0,0 +1,8 @@
+package DSALearn.mainfile;
+
+public class main1 {
+    public static void main(String[] args) {
+        System.out.println("Mera naam Amit Gupta hai");
+    }
+    
+}
