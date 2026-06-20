e36f3e88907780115239a20147a3426cd6ee97ee
Amit Gupta <amitgupta4263@gmail.com>
2026-06-20 15:59:40 +0530

    update target type

--- DSALearn/Revise/BinarySearch/BinarySearch2.java
+++ DSALearn/Revise/BinarySearch/BinarySearch2.java
@@ -2,12 +2,14 @@ package BinarySearch;
 
 public class BinarySearch2 {
     int[] arr = {5,3,5,8,7,9,5,2};
-    target = 5;
+    int target = 5;
     public static void main(String[] args) {
         BinarySearch2 obj = new BinarySearch2();
-        for(int i = 0; i<arr.length; i++){
-            if(obj.arr[i] == terget){
+        for(int i = 0; i<obj.arr.length; i++){
+            if(obj.arr[i] == obj.target){
                 return;
+                int count = 0;
+                
             }
         }
         
