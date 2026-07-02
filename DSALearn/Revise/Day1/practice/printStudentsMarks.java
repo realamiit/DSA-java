480d8a4eee0b796bdb9a0dd5cd1eb942bad915ed
Amit Gupta <amitgupta4263@gmail.com>
2026-07-02 23:42:01 +0530

    (i+1) for itrating ex 1 2 3 4 5 6

--- DSALearn/Revise/Day1/practice/printStudentsMarks.java
+++ DSALearn/Revise/Day1/practice/printStudentsMarks.java
@@ -2,10 +2,10 @@ package Day1.practice;
 
 public class printStudentsMarks {
     public static void main(String[] args) {
-    int[] arr = { 88, 89, 74, 80, 78, 94 };
-    for(int i=0; i < arr.length; i++){
-        System.out.println("Student " + 1 + " ka marks "+ arr[i]);
+        int[] arr = { 88, 89, 74, 80, 78, 94 };
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Student " + (i+1) + " ka marks " + arr[i]);
+        }
     }
-}
 
 }
