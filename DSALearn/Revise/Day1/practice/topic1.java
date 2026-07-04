113529c9eb89e6705792c6c2ca57d08770b5c928
Amit Gupta <amitgupta4263@gmail.com>
2026-07-04 23:50:28 +0530

    fix bug sum +-= num

--- DSALearn/Revise/Day1/practice/topic1.java
+++ DSALearn/Revise/Day1/practice/topic1.java
@@ -4,15 +4,19 @@ import java.util.Scanner;
 
 public class topic1 {
     public static void main(String[] args) {
+        int sum = 0;
         Scanner sc = new Scanner(System.in);
         for(int i=0; i<=4; i++){
             System.out.println("Enter your "+ (i+1) + " no");
             int num = sc.nextInt();
+            sum += num;
 
-            int sum = num+num;
-            System.out.println(sum);
-            
+           System.out.println("Sum: " + sum);
+            double average = sum / 5.0;
+            System.out.println("Average: " + average);
         }
+           
+            
         sc.close();
     }
 
