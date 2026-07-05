ac13445752854af74c523ba100d0465c81224c7c
Amit Gupta <amitgupta4263@gmail.com>
2026-07-05 17:10:58 +0530

    Add more elements to num array in findMax
    Updated the array to include additional numbers for maximum calculation.

--- DSALearn/Revise/Day1/practice/findMax.java
+++ DSALearn/Revise/Day1/practice/findMax.java
@@ -2,7 +2,7 @@ package Day1.practice;
 
 public class findMax {
     public static void main(String[] args) {
-        int[] num = {12, 45, 7, 89, 23};
+        int[] num = {12, 45, 7, 89, 23, 94, 88, 199 };
         int max = num[0];
         for(int i=0; i<num.length; i++){
             if(num[i] > max){      // if we go with < we get smallest num in array
@@ -24,3 +24,6 @@ public class findMax {
 // i=2: num[2]=7, 7 > 45? No → max stays 45
 // i=3: num[3]=89, 89 > 45? Yes → max = 89
 // i=4: num[4]=23, 23 > 89? No → max stays 89
+// i=5: num[5]=94, 94 > 45? yes → max stays 94
+// i=6: num[6]=88, 88 > 45? Yes → max = 88
+// i=4: num[7]=199, 199 > 89? yes → max stays 199
