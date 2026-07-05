d9391991a49f80466500ebd4d693a0ad8c56cc3e
Amit Gupta <amitgupta4263@gmail.com>
2026-07-05 15:00:16 +0530

    Solved Phase 1 Q3: Find max in array

---
+++ DSALearn/Revise/Day1/practice/findMax.java
@@ -0,0 +1,26 @@
+package Day1.practice;
+
+public class findMax {
+    public static void main(String[] args) {
+        int[] num = {12, 45, 7, 89, 23};
+        int max = num[0];
+        for(int i=0; i<num.length; i++){
+            if(num[i] > max){      // if we go with < we get smallest num in array
+                max=num[i]; 
+            }
+        }
+        System.out.println("Sabse bada mai hi hu malliiik muje aapne dhudh liya: " + max);
+    }
+
+}
+//  complexicity
+// time : O(n)
+// space: O(1)
+
+// Dry run:
+// max = num[0] = 12
+// i=0: num[0]=12, 12 > 12? No → max stays 12
+// i=1: num[1]=45, 45 > 12? Yes → max = 45
+// i=2: num[2]=7, 7 > 45? No → max stays 45
+// i=3: num[3]=89, 89 > 45? Yes → max = 89
+// i=4: num[4]=23, 23 > 89? No → max stays 89
