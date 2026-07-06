4bb6aa8426f5396b4d0753e94c231ec5f9bc4e27
Amit Gupta <amitgupta4263@gmail.com>
2026-07-06 23:21:43 +0530

    Solved Phase 1 Q5: Reverse array print without new array

---
+++ DSALearn/Revise/Day1/practice/reverseArrWithoutCreatingNewArr.java
@@ -0,0 +1,12 @@
+package Day1.practice;
+
+public class reverseArrWithoutCreatingNewArr {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5,};
+        for(int i=arr.length-1; i>=0; i--){
+            System.out.println(arr[i]);
+        }
+        // System.out.println(arr.length);
+    }
+
+}
