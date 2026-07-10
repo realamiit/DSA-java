2a988d93b5c3e3bee4838172a64c285e33966fd6
Amit Gupta <amitgupta4263@gmail.com>
2026-07-10 20:31:08 +0530

    Update Pattern04.java

--- DSALearn/Patterns/Pattern04.java
+++ DSALearn/Patterns/Pattern04.java
@@ -1,10 +1,10 @@
 
 public class Pattern04 {
     public static void main(String[] args) {
-        int n = 8;
+        int n = 18;
         for(int i=0; i<n-1; i++){
             for(int j=0; j<i-1; j++){
-                System.out.print("*");
+                System.out.print(i+1);
             }
             System.out.println(" ");
         }
