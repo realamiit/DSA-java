95e780c0aaccd3e7d6b190808addfe4856ed2112
Amit Gupta <amitgupta4263@gmail.com>
2026-07-15 14:38:53 +0530

    Rev: find the substring in A String

--- DSALearn/StringPractice/FindSubstringInAString.java
+++ DSALearn/StringPractice/FindSubstringInAString.java
@@ -1,14 +1,21 @@
 public class FindSubstringInAString {
     public static void main(String[] args) {
-//  it means substring is Avilable on String also than the output is true than Flass ()no
         String s1 = "follobuilding.programamit";
         String s2 = "building.program";
+        String s3 = "amit";
 
-        if(s1.contains(s2)){   //Check karta hai ki s2 continuous substring hai ya nahi
+        if (s1.contains(s2)) {
             System.out.println("True");
-        }else{
+        } else if (s1.contains(s3)) {
+            System.out.println("True");
+        } else {
             System.out.println("False");
         }
     }
 
 }
+
+// Check karta hai ki s2 continuous substring hai ya nahi
+// meanst ye conferm krta hai ki s1 me s2 ,s3 ka substring hai ya nhi
+// if substring hais2 ka to true nhi hai to bhi true because s3 hoga substring
+// agr s3 bhi nhi hoga than also go with false
