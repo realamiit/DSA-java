34b83cf80e3b9477ca0055e7118ed8c994945da8
Amit Gupta <amitgupta4263@gmail.com>
2026-07-15 14:25:32 +0530

    Revise bigest, bigiestNumInThreeNum, simpleIntrest

---
+++ DSALearn/Revise/Day1/practice/bigiestNumInThreeNum.java
@@ -0,0 +1,23 @@
+package Day1.practice;
+
+public class bigiestNumInThreeNum {
+    public static void main(String[] args) {
+        int n1=24;
+        int n2=7;
+        int n3=110;
+        if(n1>n2){
+            if(n1>n3){
+                System.out.println(n1);
+            }else{
+                System.out.println(n3);
+            }
+        }else if(n2>n3){
+            System.out.println(n2);
+        }else{
+            System.out.println(n3);
+        } 
+    }
+
+}
+
+// biggest num in 3 numbers 
