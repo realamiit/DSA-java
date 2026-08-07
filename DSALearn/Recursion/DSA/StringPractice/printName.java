edbaa8a2fb32a69f8a4a9cbc88f39fef5fdb2fbf
Amit Gupta <amitgupta4263@gmail.com>
2026-08-07 22:53:01 +0530

    printname Using Recursion

--- DSALearn/Recursion/DSA/StringPractice/printName.java
+++ DSALearn/Recursion/DSA/StringPractice/printName.java
@@ -1,11 +1,20 @@
 package StringPractice;
 
+import java.util.Scanner;
+
 public class printName {
     static void printNames(String name, int n){
         if(n<=0) return;
+        System.out.println(name);
+        printNames(name, n-1);
     }
     
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = sc.nextLine();
+        System.out.println("Enter how many times to print");
+        int n = sc.nextInt();
+        printNames(name, n);
     }
 }
