be443a3e27b79787e48d49561e70a72ba5b0d2e1
Amit Gupta <amitgupta4263@gmail.com>
2026-08-31 23:38:24 +0530

    Add pattern method to print stars
    Refactored main method to call a new pattern method that prints stars.

--- DSALearn/Patterns/Pattern05.java
+++ DSALearn/Patterns/Pattern05.java
@@ -1,7 +1,14 @@
 public class Pattern05 {
-    public static void main(String[] args) {
-        for(int i = 1; i<9; i++){
-            System.out.println();
+    public static void pattern(int n) {
+        for(int i = 1; i<=n; i++) {
+            System.out.println("*");
         }
+    }
+    public static void main(String[] args) {
+        pattern(8);
+        
+        // for(int i = 1; i<9; i++){
+        //     System.out.println();
+        // }
     }
 }
