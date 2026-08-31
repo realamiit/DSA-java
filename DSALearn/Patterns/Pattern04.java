10a4546b2284ed35a48de44cbbcdb94abaa07994
Amit Gupta <amitgupta4263@gmail.com>
2026-08-31 23:35:09 +0530

    Refactor Pattern04 to use pattern method
    Refactored main method to call pattern method and removed unused code.

--- DSALearn/Patterns/Pattern04.java
+++ DSALearn/Patterns/Pattern04.java
@@ -1,13 +1,24 @@
 
 public class Pattern04 {
-    public static void main(String[] args) {
-        int n = 18;
-        for(int i=0; i<n-1; i++){
-            for(int j=0; j<i-1; j++){
+
+    public static void pattern(int n) {
+        for(int i = 0; i <= n; i++ ) {
+            for(int j = 0; j < i-1; j++) {
                 System.out.print(i+1);
             }
             System.out.println(" ");
         }
+    }
+    public static void main(String[] args) {
+        pattern(6);
+        
+        // int n = 18;
+        // for(int i=0; i<n-1; i++){
+        //     for(int j=0; j<i-1; j++){
+        //         System.out.print(i+1);
+        //     }
+        //     System.out.println(" ");
+        // }
     }
 
 }
